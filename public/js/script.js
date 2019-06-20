275b4623d2178fa4ef46e883220342f187bc02e6
Roentgen Mark Martin <mokong@Roentgens-MacBook-Pro.local>
2019-06-20 19:34:48 -0400

    changed text box to button

--- public/js/script.js
+++ public/js/script.js
@@ -37,35 +37,26 @@ $(function() {
   const screenArea = document.querySelector('.screen-area');
   const letterCount = document.querySelector('#letter-count');
   const timer = document.querySelector('.timer');
-  const letters = ['vowel', 'consonant'];
 
   letterCount.innerHTML = charLength + " letters left";
-  $('form').submit((e) => {
-    e.preventDefault();
+
+  $('.chars').click((e) => {
     let requestedLetter = '';
-    let ans = document.querySelector('#answer').value.toLowerCase();
+    let ans = e.target.value;
     if(containers.length != 9){
-      if(ans.includes(letters[0]))
-      {
-        // TODO: generate a random vowel
-        requestedLetter = generate(letters[0]);
-        console.log('you requested a vowel: '  + requestedLetter);
-      } else if(ans.includes(letters[1])) {
-        // TODO: generate a random consonant
-        requestedLetter = generate(letters[1]);
-        console.log('you requested a consonant: ' + requestedLetter);
-      }
+      requestedLetter = generate(ans);
       screenArea.innerHTML += "<span id=\"character-\"" + containers.length + ">" + requestedLetter.toUpperCase() + "</span>";
       // store the letters in the array
       containers.push(requestedLetter);
-      console.log(containers.length);
       charLength--;
     }
+
     letterCount.innerHTML = charLength === 0 ?  "The picking phase is over" : charLength + " letters left";
 
     if(charLength === 0) {
+      $('.chars').prop('disabled', true);
       timer.innerHTML = "starting in 5 seconds";
-      const start = setTimeout(() => {setTimer(timer)}, 5000);
+      const start = setTimeout(setTimer(timer), 5000);
     }
   });
 });
