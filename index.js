421a4e9c42506e02dc2b1ddcbda360c9bc796518
Mark Martin <n01321102@humbermail.ca>
2019-06-21 00:12:59 -0400

    added emitters to picking phase, choosing letters and added a player name functionality

--- index.js
+++ index.js
@@ -1,15 +1,67 @@
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
-const socket = require("socket.io")(http);
+const io = require("socket.io")(http);
 const port = 4000;
+
+let playerCount = 0;
+const playerList = [];
 
 app.use(express.static("public"));
 
+function Player(id, name){
+  this.id = id;
+  this.name = name;
+}
+
+function Play(players, socks){
+  let connectedUsers = parseInt(io.engine.clientsCount);
+  const randomNum = Math.floor(Math.random() * connectedUsers);
+  let playerTurn = Object.keys(io.sockets.sockets)[randomNum];
+  io.to(playerTurn).emit('turn', "it's your turn");
+} 
+
 // socket
-socket.on("connection", socket => {
-  console.log("you are connected");
+io.on("connection", socket => {
+
+  socket.on('player-join', player => {
+    let newPlayer = new Player(socket.id, player);
+    playerList.push(newPlayer);
+    playerCount++;
+    if(playerCount === 1) {
+      socket.emit('waiting');
+    }
+
+    if(playerCount >= 2) {
+      io.emit('game-start');
+      Play(playerList, socket);
+    }
+  });
+
+  socket.on('player-name', player => {
+    socket.broadcast.emit('broadcast-turn', player);
+  });
+
+  socket.on('pick-letters', letter => {
+    io.emit('show-letter', letter);
+  });
+
+  socket.on('letters-left', charLength => {
+    charLength === 0 ?  "The picking phase is over" : charLength + " letters left";
+    io.emit('letters-counter', charLength);
+  });
+
+  socket.on('setTimer', time => {
+    io.emit('timer', time);
+  });
+
+  socket.on('end-round', () => {
+    console.log('round ends');
+    io.emit('check-words');
+  });
+
 });
+
 
 http.listen(port, () => {
   console.log("connected to port " + port);
--- public/js/script.js
+++ public/js/script.js
@@ -13,81 +13,118 @@ function generate(type){
 }
 
 // TODO: add timer
-function setTimer(element){
-  let timer = 30;
-  let startTimer = setInterval(() => {
-    if(timer === 0){
-      clearInterval(startTimer);
-      element.innerHTML = "Time's up!";
-      // TODO: check inputs in dictionary
-
-    } else {
-      timer--;
-      element.innerHTML = timer + ' seconds left';
-    }
-  },1000);
-}
+// function setTimer(element){
+//   let timer = 30;
+//   let startTimer = setInterval(() => {
+//     if(timer === 0){
+//       element.innerHTML = "Time's up!";
+//       clearInterval(startTimer);
+//       return false;
+//     } else {
+//       timer--;
+//       element.innerHTML = timer + ' seconds left';
+//     }
+//   },1000);
+// }
 // TODO: game logic
 // TODO: score system
-// TODO: emitting values
 
 // connection
-$(function() {
-  var socket = io();
+
+function init() {
+  var socket = io("http://localhost:4000");
   var charLength = 9;
   const containers = new Array();
   const screenArea = document.querySelector('.screen-area');
-  const chatMsg = document.querySelector('#chat-msg');
   const letterCount = document.querySelector('#letter-count');
+  const turnMsg = document.querySelector('#turn-msg');
+  const letterChoice = document.querySelector('#letter-choice');
   const timer = document.querySelector('.timer');
 
-  letterCount.innerHTML = charLength + " letters left";
+    // TODO: emitting values
+    socket.on('waiting', () => {
+      screenArea.innerHTML = "<h2>Waiting for other players to join...</h2>";
+    });
+  
+    socket.on('game-start', () => {
+      console.log('the game has started');
+      screenArea.innerHTML = '';
+    });
+
+    socket.on('turn', message => {
+      let player = document.querySelector('#player-name').innerHTML;
+      socket.emit('player-name', player);
+      screenArea.innerHTML = '';
+      letterCount.innerHTML = charLength + " letters left";
+      letterChoice.style.display = "block";
+    });
+
+    socket.on('broadcast-turn', player => {
+      turnMsg.innerHTML = 'get ready bitch, <strong>' + player + '</strong> is picking the letters';
+    });
+
+    socket.on('show-letter', letter => {
+      screenArea.innerHTML += "<span id=\"character-\"" + containers.length + ">" + letter + "</span>";
+    });
+
+    socket.on('letters-counter', counter => {
+      letterCount.innerHTML = counter;
+    });
+
+    socket.on('timer', time => {
+      timer.innerHTML = time;
+    });
+
+    socket.on('check-words', () => {
+      let letterLength = prompt('How many letters?\nEnter[0-9]');
+      letterLength = parseInt(letterLength);
+      socket.emit('letter-length', letterLength);
+    });
+
 
   $('.chars').click((e) => {
     let requestedLetter = '';
-<<<<<<< HEAD
-    let msg = document.querySelector('#answer').value;
-    let ans = msg.toLowerCase();
-
-    // add chat to window
-    chatMsg.innerHTML += "<li>" + msg + "</li>";
-
-    if(containers.length != 9){
-      if(ans.includes(letters[0]))
-      {
-        // TODO: generate a random vowel
-        requestedLetter = generate(letters[0]);
-        console.log('you requested a vowel: '  + requestedLetter);
-      } else if(ans.includes(letters[1])) {
-        // TODO: generate a random consonant
-        requestedLetter = generate(letters[1]);
-        console.log('you requested a consonant: ' + requestedLetter);
-      }
-      screenArea.innerHTML += "<span id=\"character-" + containers.length + "\">" + requestedLetter.toUpperCase() + "</span>";
-
-      if(ans.includes(letters[0]) || ans.includes(letters[1])) {
-        // store the letters in the array
-        containers.push(requestedLetter);
-        console.log(containers.length);
-        charLength--;
-      }
-=======
     let ans = e.target.value;
     if(containers.length != 9){
       requestedLetter = generate(ans);
-      screenArea.innerHTML += "<span id=\"character-\"" + containers.length + ">" + requestedLetter.toUpperCase() + "</span>";
+      socket.emit('pick-letters', requestedLetter.toUpperCase());
+      socket.emit('letters-left', charLength + " letters left");
       // store the letters in the array
       containers.push(requestedLetter);
       charLength--;
->>>>>>> 275b4623d2178fa4ef46e883220342f187bc02e6
     }
-
-    letterCount.innerHTML = charLength === 0 ?  "The picking phase is over" : charLength + " letters left";
 
     if(charLength === 0) {
+      socket.emit('letters-left', "Picking phase is over!");
       $('.chars').prop('disabled', true);
-      timer.innerHTML = "starting in 5 seconds";
-      const start = setTimeout(setTimer(timer), 5000);
+      socket.emit('setTimer', "starting in 5 seconds");
+      const start = setTimeout(() => {
+        let time = 30;
+        let startTimer = setInterval(() => {
+          if(time === 0){
+            clearInterval(startTimer);
+            socket.emit('setTimer', "Time's up!");
+            socket.emit('end-round');
+          } else {
+            time--;
+            socket.emit('setTimer', time + " seconds left");
+          }
+        },1000);
+      }, 5000);
     }
   });
-});
+
+
+  let playerName = "";
+  while(playerName === "" || playerName === null){
+    playerName = prompt('What is your name?');
+  };
+
+  if(playerName !== "" || playerName !== null){
+    playerName = playerName.trim()
+    socket.emit('player-join', playerName);
+    document.querySelector('#player-name').innerHTML = playerName;
+  }
+}
+
+window.onload = init();
