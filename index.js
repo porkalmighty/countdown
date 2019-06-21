b9afe3ac52ce9f4699d4b1dfdddb89d7f4dde62b
Mark Martin <n01321102@humbermail.ca>
2019-06-21 16:50:42 -0400

    aded scoreing system and game logic

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const axios = require('axios');
 const port = 4000;
 const playerList = [];
 
-
+var answers = 0;
 var pickingPhase = false;
 var playerCount = 0;
+var wordList = [];
 var currentGame = {
   picker: "",
   currentLetters: [],
@@ -19,6 +21,8 @@ app.use(express.static("public"));
 function Player(id, name){
   this.id = id;
   this.name = name;
+  this.letterLength = 0;
+  this.score = 0;
 }
 
 function Play(){
@@ -26,32 +30,26 @@ function Play(){
   const randomNum = Math.floor(Math.random() * connectedUsers);
   let playerTurn = Object.keys(io.sockets.sockets)[randomNum];
   io.to(playerTurn).emit('turn', "it's your turn");
-} 
+}
+
+function sortAsc(value1, value2){
+  return value1.letterLength - value2.letterLength;
+}
+
+function fetchAnagram(characters) {
+  return axios.get(`http://www.anagramica.com/all/${characters}`)
+  .then(response => {
+    return response.data.all;
+  }).catch(err => console.log(err));
+}
 
 // socket
 io.on("connection", socket => {
-
-  socket.on("disconnect", () => {
-    console.log('disconnection notice');
-    if(playerCount === 2){
-      let quitter = playerList.find(p => p.id === socket.id);
-      let index = playerList.findIndex(p => p.id === socket.id);
-      socket.broadcast.emit('rage-quit', quitter);
-      socket.broadcast.emit('waiting');
-      playerCount--;
-      playerList.splice(index, 1);
-    } else {
-      let quitter = playerList.find(p => p.id === socket.id);
-      console.log(quitter);
-      socket.broadcast.emit('quit', quitter);
-    }
-
-  });
-
   socket.on('player-join', player => {
     let newPlayer = new Player(socket.id, player);
     playerList.push(newPlayer);
     playerCount++;
+    socket.emit('join', player)
 
     if(playerCount === 1) {
       socket.emit('waiting');
@@ -91,9 +89,68 @@ io.on("connection", socket => {
     io.emit('timer', time);
   });
 
-  socket.on('end-round', () => {
+  socket.on('end-round', letters => {
     console.log('round ends');
+    // fetch anagram api
+    letters = letters.join('');
+    console.log(letters);
+    console.log('---------');
+    fetchAnagram(letters).then(data => {
+      console.log(data);
+      wordList.push(data);
+    });
     io.emit('check-words');
+  });
+
+  socket.on('letter-length', data => {
+    console.log(`this is the word length ${data}`);
+    answers++;
+    let player = playerList.find(p => p.id === socket.id);
+    player.letterLength = data;
+
+    // check if all users entered their answers
+    if(answers === playerList.length){
+      console.log(playerList.length);
+      console.log(playerList);
+      let sortedList = [...playerList];
+      let sortedUser = sortedList.sort(sortAsc).pop();
+
+      // emit answer form to the user that has the highest letter
+      io.to(sortedUser.id).emit('get-answer', sortedUser.letterLength);
+      socket.broadcast.emit('show-highest', sortedUser.name);
+    }
+  });
+
+// game logic
+// score system
+
+  socket.on('evaluate', data => {
+    let match = wordList[0].includes(data);
+    let player = playerList.find(p => p.id === socket.id);
+    if(match){
+      console.log(`matched: ${player.name}`);
+      player.score = parseInt(player.letterLength) * 10;
+      socket.broadcast.emit('ans', player);
+      io.emit('show-answers', wordList[0]);
+    }
+  });
+
+
+  // todo: add rounds
+
+  socket.on("disconnect", () => {
+    console.log('disconnection notice');
+    if(playerCount === 2){
+      let quitter = playerList.find(p => p.id === socket.id);
+      let index = playerList.findIndex(p => p.id === socket.id);
+      socket.broadcast.emit('rage-quit', quitter);
+      socket.broadcast.emit('waiting');
+      playerCount--;
+      playerList.splice(index, 1);
+    } else {
+      let quitter = playerList.find(p => p.id === socket.id);
+      socket.broadcast.emit('quit', quitter);
+    }
   });
 
 });
--- public/js/script.js
+++ public/js/script.js
@@ -12,36 +12,33 @@ function generate(type){
   return randomChar;
 }
 
-// TODO: add timer
-// function setTimer(element){
-//   let timer = 30;
-//   let startTimer = setInterval(() => {
-//     if(timer === 0){
-//       element.innerHTML = "Time's up!";
-//       clearInterval(startTimer);
-//       return false;
-//     } else {
-//       timer--;
-//       element.innerHTML = timer + ' seconds left';
-//     }
-//   },1000);
-// }
-// TODO: game logic
-// TODO: score system
-
 // connection
 
 function init() {
   var socket = io("http://localhost:4000");
   var charLength = 9;
   var startTimer;
-  const containers = new Array();
+  var containers = new Array();
   const screenArea = document.querySelector('.screen-area');
   const letterCount = document.querySelector('#letter-count');
   const announcer = document.querySelector('#announcer');
   const turnMsg = document.querySelector('#turn-msg');
   const letterChoice = document.querySelector('#letter-choice');
   const timer = document.querySelector('.timer');
+  const answerForm = document.querySelector('.form');
+  const submitBtn = document.querySelector('#send');
+  const answerKey = document.querySelector('#possible-answers');
+
+  let playerName = "";
+  while(playerName === "" || playerName === null){
+    playerName = prompt('What is your name?');
+  };
+
+  if(playerName !== "" || playerName !== null){
+    playerName = playerName.trim()
+    socket.emit('player-join', playerName);
+    document.querySelector('#player-name').innerHTML = playerName;
+  }
 
     // TODO: emitting values
     socket.on('waiting', () => {
@@ -51,10 +48,22 @@ function init() {
     socket.on('game-start', () => {
       console.log('the game has started');
       screenArea.innerHTML = '';
+      letterCount.innerHTML = '';
+      letterChoice.style.display = "none";
+      turnMsg.innerHTML = '';
+      clearInterval(startTimer);
+      timer.innerHTML = '';
+      announcer.innerHTML = "";
+      answerForm.style.display = "none";
+    });
+
+    socket.on('join', player => {
+      announcer.innerHTML = player.name + " has joined the game";
     });
 
     socket.on('quit', player => {
       announcer.innerHTML = player.name + " has left the game";
+      containers = new Array();
     });
 
     socket.on('rage-quit', player => {
@@ -64,6 +73,8 @@ function init() {
       clearInterval(startTimer);
       timer.innerHTML = '';
       announcer.innerHTML = player.name + " has left the game";
+      answerForm.style.display = "none";
+      containers = new Array();
     });
 
     socket.on('turn', message => {
@@ -79,6 +90,10 @@ function init() {
       letterCount.innerHTML = charLength + " letters left";
     });
 
+    socket.on('show-highest', player => {
+      turnMsg.innerHTML = '<strong>' + player + '</strong> has the longest word!';
+    });
+
     socket.on('letters-counter', counter => {
       letterCount.innerHTML = counter;
     });
@@ -92,9 +107,36 @@ function init() {
     });
 
     socket.on('check-words', () => {
-      let letterLength = prompt('How many letters?\nEnter[0-9]');
-      letterLength = parseInt(letterLength);
-      socket.emit('letter-length', letterLength);
+      let letterLength = "";
+      let inputStatus = false;
+      while(inputStatus === false){
+          letterLength = prompt('How many letters?\nEnter word length [4-9]');
+          letterLength = parseInt(letterLength);
+          if(letterLength > 3 && letterLength < 10){
+            socket.emit('letter-length', letterLength);
+            inputStatus = true;
+          }
+      }
+    });
+
+    socket.on('get-answer', letterLength => {
+      alert("enter your answer on the form")
+      answerForm.style.display = "block";
+      submitBtn.value = letterLength;
+    });
+
+    socket.on('ans', data => {
+      announcer.innerHTML = `${data.name} won and got ${data.score} points for answering a ${data.letterLength}-letter word!`;
+    });
+
+    socket.on('show-answers', answers => {
+      let keyAnswers = "";
+      answers.map(val => {
+        if(val.length > 3) {
+          keyAnswers += `<li> ${val} </li>`;
+        }
+      });
+      answerKey.innerHTML = `<h3>Possible Answers</h3><ul id="answers">${keyAnswers}</ul>`;
     });
 
     socket.on('new-challenger', gameStatus => {
@@ -105,6 +147,23 @@ function init() {
         screenArea.innerHTML += "<span id=\"character-\"" + length + ">" + letters + "</span>";
       });
     })
+
+
+  $('form').submit((e) => {
+    e.preventDefault();
+    let wordLength = parseInt(submitBtn.value);
+    let answer = $('#answer').val();
+    console.log(answer.length);
+    console.log(wordLength);
+    if(answer.length === wordLength){
+      console.log('match');
+      // check the word from generated results
+      socket.emit('evaluate', answer);
+    } else {
+      alert(`Your answer does not match the wordlength(${wordLength}) you specified!`);
+    }
+    $('#answer').val('');
+  });
 
 
   $('.chars').click((e) => {
@@ -124,32 +183,20 @@ function init() {
       $('.chars').prop('disabled', true);
       socket.emit('setTimer', "starting in 5 seconds");
       const start = setTimeout(() => {
-        let time = 30;
+        let time = 1;
         startTimer = setInterval(() => {
           if(time === 0){
             clearInterval(startTimer);
             socket.emit('setTimer', "Time's up!");
-            socket.emit('end-round');
+            socket.emit('end-round', containers);
           } else {
             time--;
             socket.emit('setTimer', time + " seconds left");
           }
         },1000);
-      }, 5000);
+      }, 1000);
     }
   });
-
-
-  let playerName = "";
-  while(playerName === "" || playerName === null){
-    playerName = prompt('What is your name?');
-  };
-
-  if(playerName !== "" || playerName !== null){
-    playerName = playerName.trim()
-    socket.emit('player-join', playerName);
-    document.querySelector('#player-name').innerHTML = playerName;
-  }
 }
 
 window.onload = init();
